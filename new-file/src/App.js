a1300918e42039a1b41b3b6da248c20dd1470f03
Ishika <ishikamriad03@gmail.com>
2023-04-09 17:38:36 +0530

    Initial Commit

--- new-file/src/App.js
+++ new-file/src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Analytics from "./components/Analytics";
+import Hero from "./components/Hero";
+import Cards from "./components/Cards";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Analytics />
+      <Cards />
+    </div>
+  );
 }
 
 export default App;
